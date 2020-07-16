fb0cfb3ea3f04765a4e3028b89e75f5fe25b077f
南方者 <nanfangzhe@qq.com>
2020-07-16 08:51:13 +0800

    1. 主干添加

--- service/service_store/src/main/java/com/anpai/shoesservice/ShoesApplication.java
+++ service/service_store/src/main/java/com/anpai/shoesservice/ShoesApplication.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class ShoesApplication {
     public static void main(String[] args) {
         System.out.println("我是分支");
+        System.out.println("创建主干");
         SpringApplication.run(ShoesApplication.class, args);
     }
 }
